458e66b9fdcc60f5291bdd2652e320707694cc3a
kubiesh <kubiesh@users.noreply.github.com>
2018-03-13 09:05:27 +0100

    United we stand

--- PTM-GitHub-Bandowski.cpp
+++ PTM-GitHub-Bandowski.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	cout << "Hello Git" << endl;
 	cout << "Ja, Szymon Bandowski jestem pierwszym autorem tego programu" << endl;
+	printf("United we stand, divided we fall!");
 	system("pause");
 	return 0;
 }
